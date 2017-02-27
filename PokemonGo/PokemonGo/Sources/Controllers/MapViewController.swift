69141499531368798e9cc68da535589af0a34dd8
André Luís  Campopiano <andreluiscampopiano@mac-mini-de-andre.local>
2017-02-27 15:59:20 -0300

    Arquitetura do projeto

---
+++ PokemonGo/PokemonGo/Sources/Controllers/MapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MapViewController.swift
+//  PokemonGo
+//
+//  Created by André Luís  Campopiano on 27/02/17.
+//  Copyright © 2017 André Luís  Campopiano. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
