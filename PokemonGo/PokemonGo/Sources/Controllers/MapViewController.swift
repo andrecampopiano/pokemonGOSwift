4f257ed15577b9058e21d9e069acfb645f6f1b9d
André Luís  Campopiano <andreluiscampopiano@mac-mini-de-andre.local>
2017-02-27 16:31:10 -0300

    Inclusao das permissões de acesso ao gps do usuário

--- PokemonGo/PokemonGo/Sources/Controllers/MapViewController.swift
+++ PokemonGo/PokemonGo/Sources/Controllers/MapViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var managerLocation = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        managerLocation.delegate = self
+        
+        //Solicitar localização
+        managerLocation.requestWhenInUseAuthorization()
+        managerLocation.startUpdatingLocation()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +31,26 @@ class MapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedWhenInUse {
+            let alertController = UIAlertController(title: "Permissão de localização", message: "Necessário a sua localização para começar a caçar pokemons.", preferredStyle: .alert)
+            
+            let actionConfig = UIAlertAction(title: "Abrir Configurações", style: .default, handler: { (alertConfig) in
+                if let config = NSURL(string: UIApplicationOpenSettingsURLString){
+                    UIApplication.shared.open(config as URL)
+                }
+            })
+            
+            let actionCancel = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            alertController.addAction(actionConfig)
+            alertController.addAction(actionCancel)
+            
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 
 }
 
