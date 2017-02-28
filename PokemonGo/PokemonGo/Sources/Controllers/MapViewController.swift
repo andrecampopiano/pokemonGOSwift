078743baabf23b767586e0896343f659b9878928
andrecampopiano <brocoyou@hotmail.com>
2017-02-27 21:44:24 -0300

    inclusao imagem do player

--- PokemonGo/PokemonGo/Sources/Controllers/MapViewController.swift
+++ PokemonGo/PokemonGo/Sources/Controllers/MapViewController.swift
@@ -13,7 +13,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
 
     @IBOutlet weak var mapView: MKMapView!
     var managerLocation = CLLocationManager()
-    
+    var contador:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,6 +24,38 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         managerLocation.requestWhenInUseAuthorization()
         managerLocation.startUpdatingLocation()
         
+        //Exibir pokemons
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { (timer) in
+            print("teste")
+            if let coordinate = self.managerLocation.location?.coordinate{
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = coordinate
+                annotation.coordinate.latitude += (Double(arc4random_uniform(400)) - 200) / 100000.0
+                annotation.coordinate.longitude += (Double(arc4random_uniform(400)) - 200) / 100000.0
+                self.mapView.addAnnotation(annotation)
+            }
+            
+        }
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        
+        
+        
+        
+        if annotation is MKUserLocation {
+            annotationView.image = #imageLiteral(resourceName: "player")
+        }else{
+            annotationView.image = #imageLiteral(resourceName: "pikachu-2")
+        }
+        
+        var frame = annotationView.frame
+        frame.size.height = 40
+        frame.size.width = 40
+        annotationView.frame = frame
+        return annotationView
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,16 +65,16 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        if let coordinate = managerLocation.location?.coordinate {
-            
-            let region = MKCoordinateRegionMakeWithDistance(coordinate, 200, 200)
-            
-            mapView.setRegion(region, animated: true)
+        if contador < 4 {
+            self.center()
+            contador += 1
+        }else {
+            managerLocation.stopUpdatingLocation()
         }
         
         
-        
     }
+    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status != .authorizedWhenInUse {
             let alertController = UIAlertController(title: "Permissão de localização", message: "Necessário a sua localização para começar a caçar pokemons.", preferredStyle: .alert)
@@ -60,6 +92,23 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             present(alertController, animated: true, completion: nil)
         }
     }
+    
+    func center(){
+        if let coordinate = managerLocation.location?.coordinate {
+            
+            let region = MKCoordinateRegionMakeWithDistance(coordinate, 200, 200)
+            
+            mapView.setRegion(region, animated: true)
+        }
 
+    }
+    
+    @IBAction func centerPlayer(_ sender: Any) {
+       self.center()
+    }
+    
+
+    @IBAction func openPokedex(_ sender: Any) {
+    }
 }
 
